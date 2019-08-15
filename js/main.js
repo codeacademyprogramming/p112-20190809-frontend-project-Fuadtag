8c3451f0b11a56f99e54d8de2364ccce4d8c739f
Fuadtag <Fuadjt@code.edu.az>
2019-08-15 12:33:24 +0400

    Slider section done

--- js/main.js
+++ js/main.js
@@ -7,4 +7,38 @@ $(document).ready(function() {
     $(".close-circle").click(function() {
         $(".modal").css("display", "none");
     })
+
+
+    $('.slide').owlCarousel({
+        loop: true,
+        dots: true,
+        nav: false,
+        navSpeed: 800,
+        autoplay: true,
+        transitionStyle: "fade",
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            }
+        }
+
+    })
+    $(".owl-carousel").owlCarousel();
+
+
+
+
+
+
+
+
+
+
+
 })
