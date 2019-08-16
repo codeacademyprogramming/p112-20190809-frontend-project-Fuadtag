23df81cd66e89057ecf25de4543b730b066926be
Fuadtag <Fuadjt@code.edu.az>
2019-08-16 10:22:51 +0400

    Accordion done

--- js/main.js
+++ js/main.js
@@ -30,7 +30,31 @@ $(document).ready(function() {
 
     })
     $(".owl-carousel").owlCarousel();
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+
+            this.classList.toggle("active");
+            // this.classList.toggle(".accordion-color");
+
+
+
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+
+            } else {
+                panel.style.display = "block";
+
+
+            }
+        });
+    }
+    // $(".accordion").click(function() {
+    //     $(".accordion").classname.toggle(".accordion-color")
+    // })
 
 
 
